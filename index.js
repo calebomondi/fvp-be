54199e15eaef948af09ddae601608ec9e93ff10c
Felista-Njeri <njeerikamau2@gmail.com>
2025-06-27 20:38:14 +0300

    feat: add twitter route plus configuration

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import Moralis from 'moralis';
+import session from "express-session";
 
 // Importing routes
 import homeRoute from './routes/homeRoute.js';
@@ -10,17 +11,34 @@ import vaultsRoute from './routes/vaultsRoute.js';
 import adminRouter from './routes/adminRoute.js';
 import histRoute from './routes/transacHistRoute.js';
 import pointsRoute from './routes/pointsRoute.js';
+import twitterRoutes from "./routes/twitterRoutes.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const SESSION_SECRET = process.env.SESSION_SECRET
 
 //  Middleware
-app.use(cors({
+app.use(
+  cors({
     origin: ["http://localhost:5173","https://fvp-fe.vercel.app", "https://fvp.finance"],
-}));
-app.use(express.json());
+    credentials: true,
+  }),
+  session({
+    secret: SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+    secure: false, // set to true in production with HTTPS
+    httpOnly: true,
+    maxAge: 24 * 60 * 60 * 1000, // 1 day
+  },
+  })
+);
+
+app.use(express.json({ limit: "5mb" }));
+app.use(express.urlencoded({ limit: "5mb", extended: true }));
 
 //  Routes
 app.use('/api/tokens', tokenRoute);
@@ -28,6 +46,7 @@ app.use('/api/vaults', vaultsRoute);
 app.use('/api/admin', adminRouter);
 app.use('/api/transactions', histRoute);
 app.use('/api/points', pointsRoute);
+app.use("/api/twitter", twitterRoutes);
 app.use('/', homeRoute);
 
 const startServer = async () => {
